8283991baac543680eab80fb64201c5530e137e4
Jamie Myland <jamie.myland@gmail.com>
2019-11-14 21:27:31 +0000

    Initialise dotnet core projects

---
+++ breaking-solid/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace breaking_solid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Breaking SOLID!");
+        }
+    }
+}
---
+++ making-solid/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace making_solid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Making SOLID!");
+        }
+    }
+}
